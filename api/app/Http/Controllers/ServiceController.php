01989bf7d4ee48c357527bcfe09df7b584fa3829
genezysg <genezys@gmail.com>
2016-09-02 12:31:37 -0300

    Adiciona lógica de orcamento no ServiceController

--- api/app/Http/Controllers/ServiceController.php
+++ api/app/Http/Controllers/ServiceController.php
@@ -9,11 +9,37 @@ use Illuminate\Http\Request;
 
 class ServiceController extends Controller{
 
+  public $AllServices;
 
+  public function __construct(){
+    $this->AllServices=[["id"=>"1","description"=>"compra", "value"=>100],["id"=>"2","description"=>"venda", "value"=>200],["id"=>"3","description"=>"more", "value"=>300]];
+  }
   public function index(){
-     $Services=["compra","venda"];
+     $Services=[["id"=>"1","description"=>"compra"],["id"=>"2","description"=>"venda"],["id"=>"3","description"=>"more"]];
      return response()->json($Services);
   }
 
+  public function getOrcamento(Request $request){
+     $response=["value"=>0];
+     $input=$request->only(["service1","service2"]);
+     $response["value"]=$this->calculate($input["service1"],$input["service2"]);
+     return response()->json($response);
+  }
+
+  public function calculate($service1,$service2){
+    $s1=$this->find($service1);
+    $s2=$this->find($service2);
+    return ($s1["value"]+$s2["value"]);
+  }
+
+  public function find($service){
+    foreach($this->AllServices as $a){
+      if ($a["id"]==$service){
+        return ($a);
+      }
+    }
+  }
 }
+
+
 ?>
