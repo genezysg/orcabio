981df9b64e2e0d9422946b1f29204643b18d9968
genezysg <genezys@gmail.com>
2016-09-02 21:46:21 -0300

    Integra back e frontend

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,24 @@
 var orcabioApp = angular.module('orcabioApp', []);
 
+var orcabioapi="http://api.orcabio.com/api/v1/";
 
-orcabioApp.controller('OrcabioController',function OrcabioController($scope){
-  $scope.services=[
-    {"id":1,
-      "description":"compra"},
-    {"id":2,
-      "description":"venda"}
-  ]
+orcabioApp.controller('OrcabioController',function OrcabioController($http,$scope){
+  $http.get(orcabioapi+"service").then(function(response) {
+        $scope.services=response.data;
+  });
+
+  $scope.calcularOrcamento= function(){
+
+    var pmt={};
+    if ($scope.serviceList!=null) {pmt.service1=$scope.serviceList.id};
+    if ($scope.serviceList2!=null) {pmt.service2=$scope.serviceList2.id};
+  //  if ($scope.serviceList.id!=null) service1=serviceList.id
+
+    $http.get(orcabioapi+"orcamento",{
+        params: pmt
+     }).then(function(response) {
+          $scope.orcamento=response.data.value;
+    });
+  };
+
 });
