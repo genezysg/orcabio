17281b722bb82dceb37a14c1a89f0fd830240da0
genezysg <genezys@gmail.com>
2016-09-03 17:11:13 -0300

    Adiciona as funcionalidades de acordo com o novo front

--- src/js/app.js
+++ src/js/app.js
@@ -2,23 +2,27 @@ var orcabioApp = angular.module('orcabioApp', []);
 
 var orcabioapi="http://api.orcabio.com/api/v1/";
 
-orcabioApp.controller('OrcabioController',function OrcabioController($http,$scope){
+var option=[];
+
+orcabioApp.controller('OrcabioController',function OrcabioController($http,$scope,$filter){
   $http.get(orcabioapi+"service").then(function(response) {
         $scope.services=response.data;
         $scope.services.push({description:"Escolha"})
   });
 
-  $scope.calcularOrcamento= function(){
-
-    var pmt={};
-    if ($scope.serviceList!=null) {pmt.service1=$scope.serviceList.id};
-    if ($scope.serviceList2!=null) {pmt.service2=$scope.serviceList2.id};
-  //  if ($scope.serviceList.id!=null) service1=serviceList.id
+  $scope.calcularOrcamento= function(serviceid){
+    console.log("Calculando Orcamento");
+    var pmt=[];
+    $scope.escolhidos=$filter('filter')($scope.services, {checked: true});;
+    $scope.escolhidos.map(function(escolhido){
+      pmt.push(escolhido.id);
+    });
+    console.log(pmt);
 
     $http.get(orcabioapi+"orcamento",{
-        params: pmt
+        params:{"services[]": pmt}
      }).then(function(response) {
-          $scope.orcamento=response.data.value;
+          $scope.orcamento="Valor: "+response.data.value;
     });
   };
 
